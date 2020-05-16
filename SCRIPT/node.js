fbfc15b4d1cf8501860d00f76010e2188dfbad42
abhishekbadhgotri <abhishekbadhgotri@gmail.com>
2020-05-16 09:55:16 -0400

    updated

--- SCRIPT/node.js
+++ SCRIPT/node.js
@@ -7,32 +7,6 @@ function plusSlides(n)
   showSlides(slideIndex += n);
 }
 
-function extra()
-{
-	location.replace("https://abhishekbadhgotri.github.io/Nielit-Project/AfterSubmit/index.html")
-	document.getElementById("message").textContent = "You're Successfully logged in";
-}
-
-function Submit(temp)
-{
-	if(temp==1)
-	{
-		document.getElementById("message").textContent = "Thanks for Registration";
-	}
-	else if(temp==2)
-	{
-		document.getElementById("message").textContent = "You're Successfully logged in";
-	}
-	else if(temp==3)
-	{
-		document.getElementById("message").textContent = "Thanks for the submitting your Query!";
-	}
-	else
-	{
-		document.getElementById("message").textContent = "Default message!";
-	}
-}
-
 function currentSlide(n)
 {
   showSlides(slideIndex = n);
